83138f782af58ca2b0c725b00856439e8ea778d3
Baraa Benjaballah <baraa.benjaballah@isimg.tn>
2024-10-30 19:14:35 +0100

    first commit

---
+++ testgitleaks.py
@@ -0,0 +1,24 @@
+# sensitive_data_example.py
+
+# Exemples de données sensibles pour tester Gitleaks
+
+# Clé API factice
+API_KEY = "1234567890abcdef1234567890abcdef"
+
+# Jeton d'accès factice
+GITHUB_TOKEN = "ghp_ABC123fakegithubtoken456DEF"
+
+# Mot de passe factice
+DATABASE_PASSWORD = "password123!"
+
+# Clé AWS factice
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Jeton OAuth factice
+OAUTH_TOKEN = "ya29.A0AfH6SMCZC123FakeOAuthTokenABC456DEF"
+
+# Exemple d'adresse email et numéro de carte de crédit
+EMAIL = "fakeemail@example.com"
+CREDIT_CARD = "4111 1111 1111 1111"  # Exemple de numéro de carte de crédit
+
+print("Ce fichier contient des données sensibles factices pour tester l'outil Gitleaks.")
